9b809458247d7502d94e3f0995b98b566f048d0e
Yong Su <jean.timex@gmail.com>
2022-12-14 22:44:51 -0800

    create bird manager

--- src/002-flappy-bird/app.ts
+++ src/002-flappy-bird/app.ts
@@ -1,17 +1,15 @@
 import { Assets } from "./types";
 import { BackgroundManager } from "./background_manager";
-import { Bird } from "./bird";
+import { BirdManager } from "./bird_manager";
 import { FpsMeter } from "./fps_meter";
 import { PipeManager } from "./pipe_manager";
 import { createCanvas, loadImage } from "./util";
 
-const POPULATION = 1;
-
 export class App {
   private backgroundManager: BackgroundManager;
   private pipeManager: PipeManager;
+  private birdManager: BirdManager;
   private fpsMeter: FpsMeter;
-  private currentBirds: Array<Bird>;
 
   public async run() {
     const assets = await this.loadAssets();
@@ -19,13 +17,8 @@ export class App {
 
     this.backgroundManager = new BackgroundManager(assets, canvas);
     this.pipeManager = new PipeManager(assets, canvas);
-    this.currentBirds = this.createBirds(assets, canvas);
+    this.birdManager = new BirdManager(assets, canvas, 1);
     this.fpsMeter = new FpsMeter(canvas);
-
-    // Listen for keyboard strokes
-    window.addEventListener("keydown", (event: KeyboardEvent) => {
-      this.handleKeyDown(event);
-    });
 
     this.loop(assets, canvas);
   }
@@ -49,54 +42,16 @@ export class App {
     return canvas;
   }
 
-  private createBirds(assets: Assets, canvas: HTMLCanvasElement): Array<Bird> {
-    const birds: Array<Bird> = [];
-
-    for (let i = 0; i < POPULATION; i++) {
-      const x = 50;
-      const canvasHeight = assets.background.height;
-      const y = canvasHeight / 2;
-      const bird = new Bird(assets, canvas, x, y);
-      birds.push(bird);
-    }
-
-    return birds;
-  }
-
-  private handleKeyDown(event: KeyboardEvent) {
-    if (event.code !== "Space") return;
-
-    for (const bird of this.currentBirds) {
-      bird.jump();
-    }
-  }
-
   private loop(assets: Assets, canvas: HTMLCanvasElement) {
     this.backgroundManager.update();
     this.pipeManager.update();
 
-    for (let i = this.currentBirds.length - 1; i >= 0; i--) {
-      const bird = this.currentBirds[i];
-      bird.update();
+    const pipes = this.pipeManager.getPipes();
+    this.birdManager.update(pipes);
 
-      if (bird.isOutOfScreen()) {
-        this.currentBirds.splice(i, 1);
-        continue;
-      }
-
-      const pipes = this.pipeManager.getPipes();
-      for (const pipe of pipes) {
-        if (pipe.isCollidedWithBird(bird)) {
-          this.currentBirds.splice(i, 1);
-        } else if (pipe.pass(bird.x)) {
-          bird.score++;
-        }
-      }
-    }
-
-    if (this.currentBirds.length === 0) {
+    if (this.birdManager.getAliveBirds().length === 0) {
       this.pipeManager.reset();
-      this.currentBirds = this.createBirds(assets, canvas);
+      this.birdManager.createNewGeneration();
     }
 
     this.fpsMeter.update();
